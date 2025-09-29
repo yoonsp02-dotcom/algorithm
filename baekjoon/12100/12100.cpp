ae102f901ae01e43fcc6b5678ee63efa28814bcf
YOONSANGPIL <yoonsp02@gmail.com>
2025-09-30 00:19:10 +0900

    finish 12100

--- baekjoon/12100/12100.cpp
+++ baekjoon/12100/12100.cpp
@@ -16,8 +16,9 @@ struct State {
     State(vector<vector<int>> _board, int _level) : board(_board), level(_level) {} 
 };
 
-int &getNearestInDir(const vector<vector<int>> &board, int y, int x, int dir);
-void printBoard(std::vector<std::vector<int>> &board);
+vector<int> compressLine(const vector<int> &line);
+void moveLeft(vector<vector<int>>& board);
+vector<vector<int>> rotateBoard(const vector<vector<int>> &board);
 void move(vector<vector<int>> &board, int dir);
 int getMax(const vector<vector<int>> &board);
 int game(vector<vector<int>> &board);
@@ -36,114 +37,66 @@ int main() {
             cin >> board[i][j];
         }
     }
-    cout << "move UP" << endl;
-    move(board, UP);
-    printBoard(board);
+
+    cout << game(board) << endl;
 
     return 0;
 }
-void printBoard(std::vector<std::vector<int>> &board) {
-    for (int i = 0; i < N; ++i) {
-        for (int j = 0; j < N; ++j)
-            cout << board[i][j] << " ";
-        cout << endl;
+vector<int> compressLine(const vector<int> &line) {
+    vector<int> nums;
+    for (int x : line) {
+        if (x != 0)
+            nums.push_back(x);
+    }
+    vector<int> merged;
+    for (int i = 0; i < nums.size(); ++i) {
+        if (i + 1 < nums.size() && nums[i] == nums[i + 1]) {
+            merged.push_back(nums[i] * 2);
+            i++;
+        } else {
+            merged.push_back(nums[i]);
+        }
+    }
+
+    while (merged.size() < N)
+        merged.push_back(0);
+    return merged;
+}
+void moveLeft(vector<vector<int>>& board) {
+    for (int y = 0; y < N; ++y) {
+        board[y] = compressLine(board[y]);
     }
 }
-int &getNearestInDir(vector<vector<int>> &board, int y, int x, int dir) {
-    int nextY = y + dy[dir];
-    int nextX = x + dx[dir];
-    while (board[nextY][nextX] == 0) {
-        if (nextY + dy[dir] < N && nextY + dy[dir] >= 0 &&
-            nextX + dx[dir] < N && nextX + dx[dir] >= 0) {
-            nextY += dy[dir];
-            nextX += dx[dir];
+vector<vector<int>> rotateBoard(const vector<vector<int>> &board) {
+    vector<vector<int>> ret(N, vector<int>(N));
+    for (int y = 0; y < N; ++y) {
+        for (int x = 0; x < N; ++x) {
+            ret[x][N - 1 - y] = board[y][x];
         }
-        else
-            break;
     }
-    return board[nextY][nextX];
+    return ret;
 }
 void move(vector<vector<int>> &board, int dir) {
-    vector<int> blank(N, -1);
-    if (dir == UP) {
-        for (int y = 0; y < N; ++y) {
-            for (int x = 0; x < N; ++x) {
-                if (board[y][x] == 0) {
-                    if (blank[x] == -1) blank[x] = y;
-                } else if (y != 0) {
-                    int &now = board[y][x];
-                    int &nearUp = getNearestInDir(board, y, x, dir);
-                    if (now == nearUp) {
-                        nearUp *= 2;
-                        now = 0;
-                    } else {
-                        board[blank[x]][x] = now;
-                        now = 0;
-                        blank[x]++;
-                    }
-                }
-            }
-        }
-    } else if (dir == DOWN) {
-        for (int y = N - 1; y >= 0; --y) {
-            for (int x = 0; x < N; ++x) {
-                if (board[y][x] == 0) {
-                    if (blank[x] == -1)
-                        blank[x] = y;
-                } else if (y != N - 1) {
-                    int &now = board[y][x];
-                    int &nearDown = getNearestInDir(board, y, x, dir);
-                    if (now == nearDown) {
-                        nearDown *= 2;
-                        now = 0;
-                    } else {
-                        board[blank[x]][x] = now;
-                        now = 0;
-                        blank[x]++;
-                    }
-                }
-            }
-        }
+    if (dir == LEFT) {
+        moveLeft(board);
     } else if (dir == RIGHT) {
-        for (int x = N - 1; x >= 0; --x) {
-            for (int y = 0; y < N; ++y) {
-                if (board[y][x] == 0) {
-                    if (blank[y] == -1)
-                        blank[y] = x;
-                } else if (x != N - 1) {
-                    int &now = board[y][x];
-                    int &nearRight = getNearestInDir(board, y, x, dir);
-                    if (now == nearRight) {
-                        nearRight *= 2;
-                        now = 0;
-                    } else {
-                        board[y][blank[y]] = now;
-                        now = 0;
-                        blank[y]--;
-                    }
-                }
-            }
-        }
-    } else { //dir == LEFT
-        for (int x = 0; x < N; ++x) {
-            for (int y = 0; y < N; ++y) {
-                if (board[y][x] == 0) {
-                    if (blank[y] == -1)
-                        blank[y] = x;
-                } else if (x != 0) {
-                    int &now = board[y][x];
-                    int &nearLeft = getNearestInDir(board, y, x, dir);
-                    if (now == nearLeft) {
-                        nearLeft *= 2;
-                        now = 0;
-                    } else {
-                        board[y][blank[y]] = now;
-                        now = 0;
-                        blank[y]++;
-                    }
-                }
-            }
-        }
+        board = rotateBoard(board);
+        board = rotateBoard(board);
+        moveLeft(board);
+        board = rotateBoard(board);
+        board = rotateBoard(board);
+    } else if (dir == UP) {
+        board = rotateBoard(board);
+        board = rotateBoard(board);
+        board = rotateBoard(board);
+        moveLeft(board);
+        board = rotateBoard(board);
+    } else if (dir == DOWN) {
+        board = rotateBoard(board);
+        moveLeft(board);
+        board = rotateBoard(board);
+        board = rotateBoard(board);
+        board = rotateBoard(board);
     }
 }
 int getMax(const vector<vector<int>> &board) {
@@ -155,8 +108,6 @@ int getMax(const vector<vector<int>> &board) {
     }
     return ret;
 }
-// 5번 이동 후 가장 큰 값 return
-//bfs
 int game(vector<vector<int>> &board) {
     queue<State> q;
     q.push(State(board, 0));
