523fe9a61c16dab7a25f7df84d6fd0c404fe871f
YOONSANGPIL <yoonsp02@gmail.com>
2025-09-30 00:19:31 +0900

    finish 12100

--- baekjoon/12100/12100.cpp
+++ baekjoon/12100/12100.cpp
@@ -27,8 +27,8 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
 
     cin >> N;
     vector<vector<int>> board(N, vector<int>(N));
