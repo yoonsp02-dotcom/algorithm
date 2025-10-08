3aee8ea9441b22468612fbe09414a8f519e7ef41
YOONSANGPIL <yoonsp02@gmail.com>
2025-10-08 14:02:03 +0900

    finish computer

--- baekjoon/14888/14888.cpp
+++ baekjoon/14888/14888.cpp
@@ -2,6 +2,15 @@
 
 using namespace std;
 
+const int INF = 1e9;
+int N;
+int nums[11];
+int operators[4];
+
+int calc(vector<int> masks);
+int getMax(vector<int> masks);
+int getMin(vector<int> masks);
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
@@ -9,6 +18,50 @@ int main() {
     // freopen("input.txt", "r", stdin);
     // freopen("output.txt", "w", stdout);
 
-    cout << -10 / 3 << endl;
+    cin >> N;
+    for (int i = 0; i < N; ++i)
+        cin >> nums[i];
+    for (int i = 0; i < 4; ++i)
+        cin >> operators[i];
+
+    vector<int> masks;
+    for (int i = 0; i < 4; ++i) {
+        for (int iter = 0; iter < operators[i]; ++iter) {
+            masks.push_back(i);
+        }
+    }
+
+    cout << getMax(masks) << "\n";
+    cout << getMin(masks) << "\n";
+
     return 0;
+}
+int calc(vector<int> masks) {
+    int ret = nums[0];
+    for (int i = 0; i < N - 1; ++i) {
+        if (masks[i] == 0) {
+            ret += nums[i + 1];
+        } else if (masks[i] == 1) {
+            ret -= nums[i + 1];
+        } else if (masks[i] == 2) {
+            ret *= nums[i + 1];
+        } else if (masks[i] == 3) {
+            ret /= nums[i + 1];
+        }
+    }
+    return ret;
+}
+int getMax(vector<int> masks) {
+    int ret = -INF;
+    do {
+        ret = max(ret, calc(masks));
+    } while (next_permutation(masks.begin(), masks.end()));
+    return ret;
+}
+int getMin(vector<int> masks) {
+    int ret = INF;
+    do {
+        ret = min(ret, calc(masks));
+    } while (next_permutation(masks.begin(), masks.end()));
+    return ret;
 }
---
+++ baekjoon/14890/14890.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N, L;
+vector<vector<int>> board;
+
+vector<vector<int>> turnLeft(vector<vector<int>> &board);
+bool canPass(vector<int> path);
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    cin >> N >> L;
+    board = vector<vector<int>>(N, vector<int>(N));
+    for (int i = 0; i < N; ++i)
+        for (int j = 0; j < N; ++j)
+            cin >> board[i][j];
+
+    int res = 0;
+    for (int i = 0; i < N; ++i) {
+        if (canPass(board[i])) {
+            res++;
+            cout << "i: " << i << endl;
+        }
+    }
+    // cout << "mid: " << res << endl;
+    board = turnLeft(board);
+    cout << "finish turn" << endl;
+    for (int i = 0; i < N; ++i) {
+        if (canPass(board[i])) {
+            res++;
+            cout << "i: " << i << endl;
+        }
+    }
+
+    cout << res << "\n";
+
+    return 0;
+}
+vector<vector<int>> turnLeft(vector<vector<int>> &board) {
+    vector<vector<int>> ret(N, vector<int>(N));
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            ret[i][j] = board[j][N - 1 - i];
+        }
+    }
+    return ret;
+}
+bool canPass(vector<int> path) {
+    vector<bool> isInstalled(path.size(), false);
+    deque<int> q;
+    q.push_back(0); // 항상 q.size() <= L
+    for (int now = 0; now + 1 < path.size(); ++now) {
+        if (path[now] == path[now + 1]) {
+            q.push_back(now + 1);
+            if (q.size() > L)
+                q.pop_front();
+        } else if (path[now] + 1 == path[now + 1]) {
+            if (q.size() < L)
+                return false;
+            while (!q.empty()) {
+                isInstalled[q.front()] = true;
+                q.pop_front();
+            }
+        } else if (path[now] - 1 == path[now + 1]) {
+            for (int iter = 1; iter <= L; ++iter) {
+                if (now + iter >= N)
+                    return false;
+                // 경사로를 놓을 칸들이 모두 한 칸 차이이면서 아직 설치되지 않은
+                // 칸이여야 함.
+                if (!(path[now + iter] == path[now] - 1 &&
+                      !isInstalled[now + iter]))
+                    return false;
+                isInstalled[now + iter] = true;
+            }
+            now += L - 1;
+            q.clear();
+        } else {
+            return false;
+        }
+    }
+    return true;
+}
