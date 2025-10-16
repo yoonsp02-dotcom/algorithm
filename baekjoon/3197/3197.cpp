fe2c63360baeb76174f8c044ba81121b6c46f466
YOONSANGPIL <yoonsp02@gmail.com>
2025-10-16 14:14:37 +0900

    finish computer

---
+++ baekjoon/3197/3197.cpp
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct DisjointSet {
+    vector<int> parent, rank;
+
+    DisjointSet(int n) {
+        parent.resize(n);
+        rank.resize(n, 0);
+        for (int i = 0; i < n; ++i)
+            parent[i] = i;
+    }
+
+    int find(int u) {
+        if (u == parent[u])
+            return u;
+        return parent[u] = find(parent[u]);
+    }
+
+    void merge(int u, int v) {
+        u = find(u);
+        v = find(v);
+        if (u == v)
+            return;
+        if (rank[u] < rank[v])
+            swap(u, v);
+        parent[v] = u;
+        if (rank[u] == rank[v])
+            rank[u]++;
+    }
+};
+const int dy[4] = {-1, 0, 1, 0};
+const int dx[4] = {0, 1, 0, -1};
+vector<string> lake;
+int R, C;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    cin >> R >> C;
+    lake = vector<string>(R);
+    vector<int> water, swan;
+    DisjointSet ds(R * C);
+    for (int y = 0; y < R; ++y) {
+        string row;
+        cin >> row;
+        for (int x = 0; x < C; ++x) {
+            if (row[x] == 'L') {
+                swan.push_back(getIdx(y, x));
+                row[x] = '.';
+            }
+            if (row[x] == '.') {
+                water.push_back(getIdx(y, x));
+                for (int dir = 0; dir < 4 && dir != 2; ++dir) {
+                    int ny = y + dy[dir];
+                    int nx = x + dx[dir];
+                    if (ny < 0 || ny >= C || nx < 0 || nx >= R)
+                        continue;
+                    if (lake[ny][nx] == '.') {
+                        ds.merge(getIdx(y, x), getIdx(ny, nx));
+                    }
+                }
+            }
+        }
+        lake[y] = row;
+    }
+    int day = 0;
+    while (true) {
+        vector<int> water = melting(ds, water);
+        day++;
+        if (ds.find(swan[0]) == ds.find(swan[1])) {
+            break;
+        }
+    }
+    cout << day << "\n";
+
+
+    return 0;
+}
+int getIdx(int y, int x) {
+    return y * C + x;
+}
+vector<int> melting(DisjointSet& ds, vector<int>& water) {
+    vector<int> ret;
+    for (int here : water) {
+        int hereY = here / C;
+        int hereX = here % C;
+        for (int dir = 0; dir < 4; ++dir) {
+            int ny = hereY + dy[dir];
+            int nx = hereX + dx[dir];
+            if (ny < 0 || ny >= C || nx < 0 || nx >= R) 
+                continue;
+            if (lake[ny][nx] == 'x') {
+                lake[ny][nx] = '.';
+                int meltingSpot = getIdx(ny, nx);
+                ds.merge(here, meltingSpot);
+                ret.push_back(meltingSpot);
+            }
+        }
+    }
+    return ret;
+}
